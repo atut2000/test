86a8dc881ca16d0b5b401f12a7c5e093221dc6e1
atut2000 <44915179+atut2000@users.noreply.github.com>
2019-01-09 22:27:26 +1100

    Update Untitled.py

--- Untitled.py
+++ Untitled.py
@@ -1,16 +1,17 @@
-
-# Import the flask library
+# Import flask with the request object
 from flask import Flask, request
 
-# Create your web server
+# Create the web server
 app = Flask(__name__)
 
-
-# When people visit the home page '/' use the hello_world function
-@app.route('/')
-def hello_world():
-    # Just a simple function that says Hello, World!
-    return 'Hello, World!'
+# You can message lol_bot via <your website>/lol
+@app.route('/lol')
+def lol_bot():
+    # Get the value of the 'text' query parameter
+    # request.values is a dictionary (cool!)
+    text = request.values.get('text')
+    # This bot lols at every command it gets sent!
+    return f'lol {text}'
 
 # Start the web server!
 if __name__ == '__main__':
